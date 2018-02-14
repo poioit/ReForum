6734513e94c0c4f79cad8f6611cec4274b66292b
poioit <poioitslash33@gmail.com>
2018-02-14 20:19:45 +0800

    set credentials config and database

--- config/credentials.js
+++ config/credentials.js
@@ -1,6 +1,6 @@
 module.exports = {
-  GITHUB_CLIENT_ID: '',
-  GITHUB_CLIENT_SECRET: '',
+  GITHUB_CLIENT_ID: '331c575bfb5220a7dee1',
+  GITHUB_CLIENT_SECRET: 'acc5fcc768b952382d60a27e830f1447b391ee2a',
   GITHUB_CALLBACK_URL: '',
-  DBURL: '',
+  DBURL: 'mongodb://localhost:27017/poioitstock',
 };
